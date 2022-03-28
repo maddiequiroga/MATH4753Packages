722949d7a458c4ffecd5655e81f0c92077973cbd
Madison <madison.c.quiroga-1@ou.edu>
2022-03-28 16:44:52 -0500

    mycltu() added to package

---
+++ R/mycltu.R
@@ -0,0 +1,27 @@
+#' @title mycltu()
+#'
+#' @param n sample size
+#' @param iter number of iterations
+#' @param a lower limit
+#' @param b upper limit
+#'
+#' @return a histogram of the sum of uniforms
+#' @export
+#'
+#' @examples
+#' dont run{
+#' mycltu(n = 5, iter = 10000, a = 0, b = 10)
+#' }
+mycltu=function(n,iter,a=0,b=10){
+  y=runif(n*iter,a,b)
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  w=apply(data,2,mean)
+  param=hist(w,plot=FALSE)
+  ymax=max(param$density)
+  ymax=1.1*ymax
+  hist(w,freq=FALSE,  ylim=c(0,ymax), main=paste("Histogram of sample mean",
+                                                 "\n", "sample size= ",n,sep=""),xlab="Sample mean")
+  lines(density(w),col="Blue",lwd=3)
+  curve(dnorm(x,mean=(a+b)/2,sd=(b-a)/(sqrt(12*n))),add=TRUE,col="Red",lty=2,lwd=3)
+  curve(dunif(x,a,b),add=TRUE,lwd=4)
+}
