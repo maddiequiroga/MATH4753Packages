3171a8d90c7168e2bec18d42fd8d201ad0182ab2
Madison <madison.c.quiroga-1@ou.edu>
2022-03-30 20:08:39 -0500

    ddt documentation

---
+++ R/ddt.R
@@ -0,0 +1,15 @@
+#' DDT data set
+#'
+#' 144 lines of measurements for assorted fish
+#'
+#' @format A data frame with 144 rows and 6 variables:
+#' \describe{
+#'   \item{RIVER}{priver fish was found in}
+#'   \item{MILE}{mile fish was found at}
+#'   \item{SPECIES}{species of fish}
+#'   \item{LENGTH}{length of fish}
+#'   \item{WEIGHT}{weight of fish}
+#'   \item{DDT}{ddt of fish}
+#' }
+#' @source Textbook
+"ddt"
